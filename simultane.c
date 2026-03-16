3ee8d03ef936bd42a896fbd8003b18579d26cde2
Elena Anfilofeva <elenanfilofeva@gmail.com>
2026-03-16 15:57:24 +0100

    Add simultane.c (ex1 var1 TP5)

---
+++ simultane.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(void)
+{
+
+    pid_t pid1, pid2, pid3;
+
+    pid1 = fork();
+    if (pid1 == 0) {
+        int error = execlp("du", "du", "-sh", "/usr/share", NULL);
+        if (error == -1)
+        {
+            perror("du fail");
+            exit(-1);
+        }
+    }
+
+    pid2 = fork();
+    if (pid2 == 0) {
+        int error = execlp("ls", "ls", "-l", NULL);
+        if (error == -1)
+        {
+            perror("ls fail");
+            exit(-1);
+        }
+    }
+
+
+    pid3 = fork();
+    if (pid3 == 0) {
+        int error = execlp("ps", "ps", NULL);
+        if (error == -1)
+        {
+            perror("ls fail");
+            exit(-1);
+        }
+    }
+    wait(NULL);
+    wait(NULL);
+    wait(NULL);
+
+
+    return 0;
+}
