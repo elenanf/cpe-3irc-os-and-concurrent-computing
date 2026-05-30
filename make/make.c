f4e17346522198c1461fda1712cd7cb5c8057bda
Elena Anfilofeva <elenanfilofeva@gmail.com>
2026-05-30 12:48:05 +0200

    Add error handling to make.c

--- make/make.c
+++ make/make.c
@@ -4,38 +4,58 @@
 #include <string.h>
 #include <sys/wait.h>
 
+int ends_with_c(const char* filename) {
+    size_t len = strlen(filename);
+    return len >= 2 && filename[len-2] == '.' && filename[len-1] == 'c';
+}
+
 int main(int argc, char* argv[])
 {
     char tab[argc-2][1024];
-    for(int i = 1; (i-1) < (argc - 2); i++) {
-        strcpy(tab[i-1], argv[i]);
+    int obj_count = 0;
+
+    for(int i = 1; i < (argc - 1); i++) {
+        if (!ends_with_c(argv[i])) continue;
+
+        strcpy(tab[obj_count], argv[i]);
+        obj_count++;
+        
         if (fork() == 0) {
-            char* arguments[4] = {"gcc","-c", argv[i], NULL};
+            char* arguments[] = {"gcc",  "-Wall", "-Wextra", "-c", argv[i], NULL};
             int error = execvp("gcc",arguments);
             if (error == -1)
             {
-                printf("Compilation error with the following file: %s", argv[i]);
+                perror("execvp failed");
                 exit(-1);
             }
         }
     }
 
-    for(int i = 1; (i-1) < (argc - 2); i++) {
-        wait(NULL);
+    int has_error = 0;
+    for (int i = 0; i < obj_count; i++) {
+        int status;
+        wait(&status);
+        if (WEXITSTATUS(status) != 0)
+            has_error = 1;
     }
 
-    char* args[argc + 2];
+    if (has_error) {
+        printf("Compilation error.\n");
+        exit(-1);
+    }
+
+    char* args[obj_count + 4];
     args[0] = "gcc";
-    int count = 1;
-    for(int i = 1; i < (argc - 1); i++) {
-        tab[i-1][strlen(tab[i-1]) - 1] = 'o';
-        args[i] = tab[i-1];
-        count++;
+
+    for(int i = 0; i < obj_count; i++) {
+        tab[i][strlen(tab[i]) - 1] = 'o';
+        args[i + 1] = tab[i];
     }
     
-    args[count] = "-o";
-    args[count + 1] = argv[argc-1];
-    args[count + 2] = NULL;
+    args[obj_count + 1] = "-o";
+    args[obj_count + 2] = argv[argc - 1];
+    args[obj_count + 3] = NULL;
+
     
     int error1 = execvp("gcc", args);
     if (error1 == -1)
